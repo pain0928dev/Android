0652545c5a3e165dfb5849775bb99b8371fd19c4
ljh0928 <ljh0928@cellumed.co.kr>
2018-01-29 16:14:14 +0900

    변수이름 정리, scan 리스트 관리,

---
+++ app/src/main/java/healthcare/cellumed/ble_test2/BluetoothLE/BLEDevice.java
@@ -0,0 +1,8 @@
+package healthcare.cellumed.ble_test2.BluetoothLE;
+
+/**
+ * Created by ljh0928 on 2018. 1. 25..
+ */
+
+public class BLEDevice {
+}
---
+++ app/src/main/java/healthcare/cellumed/ble_test2/Util/RingByteBuffer.java
@@ -0,0 +1,8 @@
+package healthcare.cellumed.ble_test2.Util;
+
+/**
+ * Created by ljh0928 on 2018. 1. 25..
+ */
+
+public class RingByteBuffer {
+}
